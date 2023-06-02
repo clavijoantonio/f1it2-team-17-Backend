88da2568414404c569023cb1f814cd50ac67f6c9
57320 <57320@LAPTOP-NH3B6M9T>
2023-06-01 22:02:02 -0500

    project initialization

---
+++ src/main/java/com/artGallery/Evoca/EvocaApplication.java
@@ -0,0 +1,13 @@
+package com.artGallery.Evoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artGallery/Evoca/EvocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artGallery.Evoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
